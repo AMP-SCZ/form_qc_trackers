77f6a113e04fb0eb754de36027b33aad32d5d40b
owenborders <owen.borders2@gmail.com>
2024-02-11 13:36:36 -0500

    moved 21 day tracker function here

--- classes/compile_errors.py
+++ classes/compile_errors.py
@@ -1,15 +1,21 @@
 import pandas as pd
 import datetime
+import os
+import openpyxl
+from openpyxl.utils import range_boundaries,get_column_letter
+from openpyxl.worksheet.dimensions import ColumnDimension
+from openpyxl import load_workbook
 
 class CompileErrors():
     """Class to format errors
     into final output as they
     are detected"""
-    
+
     def __init__(self,timepoint,variable_translation_dict):
         self.variable_translation_dict = variable_translation_dict
         self.error_dictionary = {}
         self.timepoint = timepoint
+        self.twentyone_day_tracker = []
 
     def append_error(self,row, message,  variable, form,sheet_list = ['Main Report']):
         """Function to append errors to a dictionary and collect
@@ -87,3 +93,41 @@ class CompileErrors():
                     formatted_output[sheet_name].append(form_data)
 
         return formatted_output
+
+    def create_twenty_one_day_tracker(self,absolute_path):
+        """Creates tracker to warn sites
+        if they are approaching or exceeding 21
+        days since screening psychs"""
+
+        self.twentyone_day_tracker = sorted(self.twentyone_day_tracker,\
+        key=lambda x: int(str(x['time_since_screening_psychs']).split(' ')[0]))
+        filename = f'{absolute_path}site_outputs/PRONET/combined/PRONET_Output.xlsx'
+        xls = pd.ExcelFile(filename)
+        sheetname = 'Twenty One Day Tracker'
+        twentyone_day_tracker_df = pd.DataFrame(self.twentyone_day_tracker)
+        if os.path.exists(filename) and sheetname\
+        in pd.ExcelFile(filename).sheet_names:
+            with pd.ExcelWriter(filename, mode='a', engine='openpyxl',\
+            if_sheet_exists = 'replace') as writer:
+                old_df = pd.read_excel(filename,sheet_name=sheetname)
+                for old_row in old_df.itertuples():
+                    for new_row in twentyone_day_tracker_df.itertuples():
+                        if old_row.subject == new_row.subject and\
+                        old_row.recent_baseline_assessment\
+                        == new_row.recent_baseline_assessment:
+                            if hasattr(old_row,'sent_to_site')\
+                             and hasattr(old_row,'manually_resolved'):
+                                twentyone_day_tracker_df.at[new_row.Index,\
+                                'sent_to_site'] = old_row.sent_to_site
+                                twentyone_day_tracker_df.at[new_row.Index,\
+                                'manually_resolved'] = old_row.manually_resolved
+                twentyone_day_tracker_df.to_excel(writer, sheet_name=sheetname, index=False)
+            workbook = load_workbook(filename = filename)
+            worksheet = workbook[sheetname]
+            for column in worksheet.columns:
+                column_letter = get_column_letter(column[0].column)
+                worksheet.column_dimensions[column_letter].width= 30
+            workbook.save(filename)
+        elif os.path.exists(filename):
+            with pd.ExcelWriter(filename, mode='a', engine='openpyxl') as writer:
+                twentyone_day_tracker_df.to_excel(writer, sheet_name=sheetname, index=False)
