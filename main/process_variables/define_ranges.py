fb94a5edf960bbf702045e5bfd779426051ecf44
owenborders <owen.borders2@gmail.com>
2025-07-03 04:51:56 -0400

    added list of vars to check for negative values

--- main/process_variables/define_ranges.py
+++ main/process_variables/define_ranges.py
@@ -22,6 +22,17 @@ class RangeDefiner():
         
         self.comparative_values_dict = {}
         self.collect_ranges()
+        self.negative_values_list = ['chrpharm_med2_dosage_past',
+                                     'chrblood_spintemp', 'chrscid_e207_note',
+                                     'chrscid_b11_note', 'chrgfss_prompt1', 'chrchs_bodytemp',
+                                     'chrchs_bmi', 'chrscid_e8_note', 'chrpsychs_av_audio_expl',
+                                     'chrblood_spintempc', 'chrchs_weightkg', 'chriq_tscore_sum',
+                                     'chrpps_mage', 'chrspeech_comments', 'chrpharm_med4_dosage_past',
+                                     'chriq_scaled_sum', 'chrpps_postalch', 'chrsaliva_freezer_id', 'chrchs_bodytempf',
+                                     'chrblood_serum_freeze', 'chrcssrsb_comments', 'chrpharm_med3_dosage_past', 'chrchs_heightcm',
+                                     'chrscid_d61_d65', 'chrblood_wb4pos', 'chrpps_fage', 'chrblood_wholeblood_freeze', 'chrgfr_prompt3c',
+                                     'chrblood_plasma_freeze', 'chrap_qtpdoseeq1', 'chrap_qtpdosecourse1', 'chrblood_freezerid',
+                                     'chrgfssfu_prompt1', 'chrpharm_med1_dosage_past', 'chrap_total']
 
     def __call__(self):
         return self.ranges_dict
