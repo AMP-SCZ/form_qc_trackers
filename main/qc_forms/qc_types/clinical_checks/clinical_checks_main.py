5b8117be85b889273a5abbccf021c3fb7c44b977
owenborders <owen.borders2@gmail.com>
2025-07-10 09:25:04 -0400

    made sure date are not missing codes

--- main/qc_forms/qc_types/clinical_checks/clinical_checks_main.py
+++ main/qc_forms/qc_types/clinical_checks/clinical_checks_main.py
@@ -680,23 +680,24 @@ class ClinicalChecksMain(FormCheck):
                     
     def check_onset_offset_dates(self, row):
         for med_count in range(0,61):
-            for pharm_tp_str in ["","_past"]
-            onset_var = f"chrpharm_med{med_count}_onset{pharm_tp_str}"
-            offset_var = f"chrpharm_med{med_count}_offset{pharm_tp_str}"
-            if (hasattr(row, onset_var) and hasattr(row,offset_var)):
-                onset_val = str(getattr(row, onset_var)).split(' ')[0]
-                offset_val = str(getattr(row, offset_var)).split(' ')[0]
-                if all(self.utils.check_if_val_date_format(date_val)
-                for date_val in [onset_val, offset_val]):
-                    if (datetime.strptime(onset_val, '%Y-%m-%d')
-                    > datetime.strptime(offset_val, '%Y-%m-%d')):
-                        error_message = (f"Onset date ({onset_var} = {onset_val})"
-                        f" is later than offset date ({offset_var} = {offset_val})")
-                        output_changes = {'reports' : ['Main Report']}
-                        error_output = self.create_row_output(
-                        row, [form], [name_var],
-                        error_message, output_changes)
-                        self.final_output_list.append(error_output)
+            for pharm_tp_str in ["","_past"]:
+                onset_var = f"chrpharm_med{med_count}_onset{pharm_tp_str}"
+                offset_var = f"chrpharm_med{med_count}_offset{pharm_tp_str}"
+                if (hasattr(row, onset_var) and hasattr(row,offset_var)):
+                    onset_val = str(getattr(row, onset_var)).split(' ')[0]
+                    offset_val = str(getattr(row, offset_var)).split(' ')[0]
+                    if all(self.utils.check_if_val_date_format(date_val) and 
+                    date_val not in self.utils.missing_code_list
+                    for date_val in [onset_val, offset_val]):
+                        if (datetime.strptime(onset_val, '%Y-%m-%d')
+                        > datetime.strptime(offset_val, '%Y-%m-%d')):
+                            error_message = (f"Onset date ({onset_var} = {onset_val})"
+                            f" is later than offset date ({offset_var} = {offset_val})")
+                            output_changes = {'reports' : ['Main Report']}
+                            error_output = self.create_row_output(
+                            row, [form], [name_var],
+                            error_message, output_changes)
+                            self.final_output_list.append(error_output)
 
 
 
